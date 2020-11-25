ddfc028f8e545e60b651c4033f12e9eba78f6e19
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-24 23:36:46 -0600

    commit tree 2146506c5a5d0d53bbd1b83a8fc9bff40fef163c

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Desi"
+    end 
+
+    get '/hometown' do
+        "My hometown is Corpus Christi"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is 'I'm Not Okay' by My Chemical Romance"
+    end 
+    
 end
